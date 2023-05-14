203da6f950986bd4a16725b33ac2df231359ff99
NIkolay <kchucanon@gmail.com>
2023-05-14 13:18:55 +0300

    2.30

--- src/Task2_30.java
+++ src/Task2_30.java
@@ -5,13 +5,16 @@ public class Task2_30 implements Task{
             return data.get(unknown);
         }
 
-        if(unknown.equals("Rb")) {
-            return (data.get("P") * Math.cos(data.get("Pangle")*(Math.PI/180))) / 2;
+        switch (unknown) {
+            case "Rb" -> {
+                return (data.get("P") * Math.cos(data.get("Pangle")*(Math.PI/180))) / 2;
+            }
+            case "Ra" -> {
+                return Math.sqrt(Math.pow(data.get("P")*Math.sin(data.get("Pangle")*(Math.PI/180))
+                - (data.get("P") * Math.cos(data.get("Pangle")*(Math.PI/180))) / 2, 2)
+                + Math.pow(data.get("P") * Math.cos(data.get("Pangle")*(Math.PI/180)), 2));
+            }
+            default -> {return 0;}
         }
-
-
-
-
-        return 0;
     }
 }
